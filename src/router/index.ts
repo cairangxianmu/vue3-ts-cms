a2ea672872150648a9bd95cf9829d5bfc6b26743
crxm <cairangxianmu@163.com>
2022-07-25 21:46:11 +0800

    feat(login main): add login main

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    redirect: "/login",
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    component: () => import("@/views/login/IndexLogin.vue"),
+  },
+  {
+    path: "/main",
+    component: () => import("@/views/main/IndexMain.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
